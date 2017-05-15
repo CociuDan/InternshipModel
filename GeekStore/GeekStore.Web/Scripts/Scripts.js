5f485a6e983f2681e1c7e3071fd2f49faa437d9d
dan.cociu <dan.cociu@MDDSK40036>
2017-05-15 18:54:48 +0300

    Some minor changes

---
+++ GeekStore/GeekStore.Web/Scripts/Scripts.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $('#btnClear').click(function () {
+        var elements = document.getElementsByTagName("input");
+        for (var ii = 0; ii < elements.length; ii++) {
+            if (elements[ii].type == "text" || elements[ii].type == "password") {
+                elements[ii].value = "";
+            }
+        }
+    });
+});
